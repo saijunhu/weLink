a13decf18f66c34e16d6b83940e46d064f4dcb73
BMR731 <1547963731@qq.com>
2016-08-24 10:18:47 +0800

    Initial Commit

---
+++ weLink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weLink
+//
+//  Created by 胡胡赛军 on 8/24/16.
+//  Copyright © 2016 胡胡赛军. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ weLinkTests/weLinkTests.swift
@@ -0,0 +1,36 @@
+//
+//  weLinkTests.swift
+//  weLinkTests
+//
+//  Created by 胡胡赛军 on 8/24/16.
+//  Copyright © 2016 胡胡赛军. All rights reserved.
+//
+
+import XCTest
+@testable import weLink
+
+class weLinkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
